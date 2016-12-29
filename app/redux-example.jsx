12a3ffa0c09186b3c8858f0e5d56801c298856be
Hung Vo <vqhung@gmail.com>
2016-12-29 13:11:50 +0700

    Add support for location fetching

--- app/redux-example.jsx
+++ app/redux-example.jsx
@@ -1,5 +1,7 @@
 var redux = require('redux');
 
+var axios = require('axios');
+
 console.log('Starting redux example');
 
 // Name reducer and action generators
@@ -94,10 +96,56 @@ var removeMovie = (id) => {
   };
 };
 
+// Map reducer and action generators
+// ----------------------------------
+
+var mapReducer = (state = {isFetching: false, url: undefined}, action) => {
+  switch (action.type) {
+    case 'START_LOCATION_FETCHING':
+      return {
+          isFetching: true,
+          url: undefined
+      };
+    case 'COMPLETE_LOCATION_FETCHING':
+      return {
+          isFetching: false,
+          url: action.url
+      };
+    default:
+      return state;
+  }
+};
+
+var startLocationFetch = () => {
+  return {
+    type: 'START_LOCATION_FETCHING'
+  };
+};
+
+var completeLocationFetch = (url) => {
+  return {
+    type: 'COMPLETE_LOCATION_FETCHING',
+    url
+  };
+};
+
+var fetchLocation = () => {
+  store.dispatch(startLocationFetch());
+
+  axios.get('http://ipinfo.io').then((res) => {
+    var loc = res.data.loc;
+    var baseUrl = 'http://maps.google.com?q='
+
+    store.dispatch(completeLocationFetch(baseUrl+loc));
+  });
+
+};
+
 var reducer = redux.combineReducers({
   name: nameReducer,
   hobbies: hobbyReducer,
-  movies: movieReducer
+  movies: movieReducer,
+  map: mapReducer
 })
 
 var store = redux.createStore(reducer, redux.compose(
@@ -108,13 +156,16 @@ var unsubscribe = store.subscribe(() =>{
   var state = store.getState();
 
   console.log('Name is', state.name);
-  document.getElementById('app').innerHTML = state.name;
 
-  console.log('New state ', store.getState());
+  if (state.map.isFetching) {
+    document.getElementById('app').innerHTML = 'Loading...';
+  } else if (state.map.url) {
+    document.getElementById('app').innerHTML = "<a href='" +state.map.url+ "'target='_blank'>View Your Location</a>";
+  }
 
 });
 
-
+fetchLocation();
 
 store.dispatch(changeName('Hung Vo'));
 
